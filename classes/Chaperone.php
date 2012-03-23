f7626a16d4a41c308f6c2707db02a0bdd8378753
Steve Criddle <steve@3stepsbeyond.co.uk>
2012-03-23 09:46:54 +0000

    Chaperone::getSchema() changed to a constant within Chaperone
    Removed default namespace
    
    Refactored to extend from ChaperoneCurrentSession.  Removed pass-through methods

--- classes/Chaperone.php
+++ classes/Chaperone.php
@@ -1,16 +1,13 @@
 <?php
-require_once('ChaperoneSession.php');
-class Chaperone {
-    
+require_once('ChaperoneCurrentSession.php');
+class Chaperone extends ChaperoneCurrentSession {
+
+    // Schema where Chaperone tables are stored.
+    const databaseSchema='global';
+
     // Database PDO for operations
     private static $PDO = NULL;
     
-    // Schema where Chaperone tables are stored.
-    private static $databaseSchema = 'global';
-    
-    // Dirty hack for now, since we only have one namespace
-    private static $namespace = 'b2b';
-
     /*
      * Setter and getter for PDO (saves us passing it around)
      */
@@ -23,44 +20,20 @@ class Chaperone {
         return self::$PDO;
     }
     
+    /*
+     * Allows a namespace to be set.  This allows you to specify contexts
+     * without namespaces when attaching roles or checking actions.
+     */
     public static function setNamespace($namespace) {
         require_once('ChaperoneNamespace.php');
         ChaperoneNamespace::setNamespace($namespace);
     }
     
+    /*
+     * Returns the current namespace
+     */
     public static function getNamespace() {
         return ChaperoneNamespace::getNamespace();
     }
-    
-    public static function getSchema() { return self::$databaseSchema; }
-    
-    public static function clearSession() {
-        ChaperoneSession::getSession()->clear();
-    }
-
-    public static function setEmailAddress($email) {
-        ChaperoneSession::getSession()->setEmailAddress($email);
-    }
-
-    public static function getEmailAddress() {
-        return ChaperoneSession::getSession()->getEmailAddress();
-    }
-
-    public static function isLoggedIn() {
-        return ChaperoneSession::getSession()->isLoggedIn();
-    }
-    
-    public static function attachRole($role, $contextArray=array()) {
-        ChaperoneSession::getSession()->attachRole($role, $contextArray);
-    }
-    
-    public static function isActionAllowed($action, $contextArray=array()) {
-        return ChaperoneSession::getSession()->isActionAllowed($action, $contextArray);
-    }
-
-    public static function getContextValueList($action, $contextItem, $contextArray=array()) {
-        return ChaperoneSession::getSession()->getContextValueList($action, $contextItem, $contextArray);
-    }
-
 }
 ?>
