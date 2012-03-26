615adf098eba189a0a5a32c58e338e178a8a7d46
Steve Criddle <steve@3stepsbeyond.co.uk>
2012-03-26 16:03:08 +0100

    100% code coverage in unit testing

--- test/ChaperoneTest.php
+++ test/ChaperoneTest.php
@@ -2,15 +2,16 @@
 require_once('../classes/ChaperoneRole.php');
 class ChaperoneTest extends PHPUnit_Framework_TestCase
 {
-    function testPDO() {
+    public function testPDO() {
         
         /*
          * Uninitialised PDO should cause an exception
          */
+        Chaperone::resetPDO();
         try {
             $fail = Chaperone::getPDO();
             $this->fail('getPDO failed to generate an exception');
-        } catch (Exception $e) {
+        } catch (ChaperoneException $e) {
             $this->assertEquals($e->getMessage(), 'Chaperone PDO has not been set');
         }
 
@@ -23,37 +24,13 @@ class ChaperoneTest extends PHPUnit_Framework_TestCase
     /*
      * Schema value is currently hard-coded
      */
-    function testSchema() {
-        $this->assertEquals(Chaperone::getSchema(), 'global');
+    public function testSchema() {
+        $this->assertEquals(Chaperone::databaseSchema, 'global');
     }
-
     
-    /*
-     * 
-     */
-    function testSplitResourceName() {
-
-        // With no namespace, return the resource name with the current namespace ID
-        $this->assertEquals(Chaperone::splitResourceName('testing'), array('namespaceId'=>1, 'namespace'=>'b2b', 'resourceName'=>'testing'));
-
-        // With the current namespace, return the current namespace ID and the resource name
-        $this->assertEquals(Chaperone::splitResourceName('b2b.testing'), array('namespaceId'=>1, 'namespace'=>'b2b', 'resourceName'=>'testing'));
-
-        // With an unrecognised namespace, expect an exception
-        try {
-            $fail = Chaperone::splitResourceName('foo.testing');
-            $this->fail('Chaperone::splitResourceName() failed to generate an exception');
-        } catch (Exception $e) {
-            $this->assertEquals($e->getMessage(), 'Namespace lookup currently unsupported');
-        }
-        
-        // If there is more than one dot, that's an error
-        try {
-            $fail = Chaperone::splitResourceName('this.should.fail');
-            $this->fail('Chaperone::splitResourceName() failed to generate an exception');
-        } catch (Exception $e) {
-            $this->assertEquals($e->getMessage(), 'Invalid Resource name "this.should.fail"');
-        }
+    public function testNamespace() {
+        Chaperone::setNamespace('testing');
+        $this->assertEquals(Chaperone::getNamespace(), 'testing');
     }
 }
 ?>
