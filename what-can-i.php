8d2e75e941c37e05fbfb285bf2c8c5d93cee7f6b
Steve Criddle <steve@3stepsbeyond.co.uk>
2012-03-27 16:54:25 +0100

    Updated for better demonstration

--- what-can-i.php
+++ what-can-i.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" type="text/css" href="chaperone.css" />
 <?php
 require_once('classes/Chaperone.php');
 
@@ -5,21 +6,50 @@ require_once('classes/Chaperone.php');
 if (!Chaperone::isLoggedIn()) die('Not logged in');
 
 // Who am I?
-echo '<b>User: </b>'.Chaperone::getEmailAddress().'<br />';
+echo '<b>User: </b>'.Chaperone::getEmailAddress().'<p />';
 
-// Within the TMC 'abc', which businesses can I do biz_view on?
-$action = 'b2b.biz_view';
-$contextArray = array('tmc'=>'abc');
-$contextItem = 'business';
+// Test items
+Chaperone::setNamespace('b2b');
 
-$contextListObj = Chaperone::getContextValueList($action, $contextItem, $contextArray);
-if ($contextListObj->isWildcard()) {
-    echo 'All!';
-} else {
-    if ($contextListObj->isEmpty()) {
-        echo 'None';
+echo '<table><tr><th>Action</th><th>Context Item</th><th>Context</th><th>Context Filter</th></tr>';
+actionFilter('tmc_view', 'tmc');                                                // Which TMCs can I view?
+actionFilter('biz_view', 'business', array('tmc'=>'abc'));                      // Which businesses can I view in TMC "abc"
+actionFilter('biz_view', 'business', array('tmc'=>'def'));                      // Which businesses can I view in TMC "def"
+actionFilter('biz_view', 'business', array('tmc'=>'ghi'));                      // Which businesses can I view in TMC "ghi"
+actionFilter('biz_view', 'business', array('tmc'=>'jkl'));                      // Which businesses can I view in TMC "jkl"
+echo '</table>';
+
+function actionFilter($action, $contextItem, $contextArray=array()) {
+
+    if (count($contextArray) === 0) {
+        $contextString = '- None -';
     } else {
-        var_dump($contextListObj->getItems());
+        $contextStringArray = array();
+        foreach ($contextArray AS $key=>$value) {
+            $contextStringArray[] = $key.'='.$value;
+        }
+        $contextString = htmlentities(join(', ', $contextStringArray));
     }
+
+    $contextFilterObj = Chaperone::getContextFilter($action, $contextItem, $contextArray);
+    if ($contextFilterObj->isWildcard()) {
+        $contextFilterList = '- All -';
+    } else {
+        if ($contextFilterObj->isEmpty()) {
+            $contextFilterList = '- None -';
+        } else {
+            $contextFilterList = htmlentities(join(',', $contextFilterObj->getItems()));
+        }
+    }
+    
+    echo '<tr><td>';
+    echo htmlentities($action);
+    echo '</td><td>';
+    echo htmlentities($contextItem);
+    echo '</td><td>';
+    echo $contextString;
+    echo '</td><td>';
+    echo $contextFilterList;
+    echo '</td></tr>';
 }
 ?>
