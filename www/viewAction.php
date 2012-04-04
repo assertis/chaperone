14eb0c44573cc2465639a4715ef13ba66d323c11
Steve Criddle <steve@3stepsbeyond.co.uk>
2012-04-04 15:00:06 +0100

    Completed functionality so that you can see all Roles for a given Action

--- www/viewAction.php
+++ www/viewAction.php
@@ -11,18 +11,49 @@ try {
     Chaperone::setPDO($pdo);
     $actionObj = ChaperoneAction::loadById($_GET['id']);
 
-    $roleRules = nl2br($actionObj->getReadableRules());
-    
+    $ruleArray = explode("\n", $actionObj->getReadableRules());
+
+    $roleArray = $actionObj->getRoles();
 } catch (Exception $e) {
     die($e);
 }
 ?><html>
-    <head><title>Action "<?php echo htmlspecialchars($actionObj->getFullName()); ?>"</title></head>
+    <link rel="stylesheet" type="text/css" href="chaperone.css" />
+    <head><title>Chaperone Action: <?php echo htmlspecialchars($actionObj->getFullName()); ?></title></head>
     <body>
-        <h1>Action "<?php echo htmlspecialchars($actionObj->getFullName()); ?>"</h1>
-        <h3>Rules</h3>
-        <?php echo $roleRules; ?>
-        
-        <h3>Roles</h3>
+        <h1>Chaperone Action: <?php echo htmlspecialchars($actionObj->getFullName()); ?></h1>
+        <table>
+            <tr><th>Rules</th></tr>
+        <?php
+            if (count($ruleArray) === 0) {
+        ?>
+            <tr><td>- No Rules</td></tr>
+        <?php
+            } else {
+                foreach ($ruleArray AS $ruleSet) {
+        ?>
+            <tr><td><?php echo htmlentities($ruleSet); ?></td></tr>
+        <?php
+                }
+            }
+        ?>
+        </table>
+        <p />
+        <table>
+            <tr><th>Roles</th></tr>
+        <?php
+            if (count($roleArray) === 0) {
+        ?>
+            <tr><td>- No Roles</td></tr>
+        <?php
+            } else {
+                foreach ($roleArray AS $roleRow) {
+        ?>
+            <tr><td><?php echo htmlentities($roleRow['role']); ?></td></tr>
+        <?php
+                }
+            }
+        ?>
+        </table>
     </body>
 </html>
