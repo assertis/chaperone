8f3fdcea93cf22bd73747499cb2c56835101f9b4
Steve Criddle <steve@3stepsbeyond.co.uk>
2012-04-04 15:00:35 +0100

    Updated to reflect the change of "business" to "biz"

--- www/attachRole.php
+++ www/attachRole.php
@@ -1,12 +1,6 @@
 <?php
+require_once('config.php');
 require_once('../classes/Chaperone.php');
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-define('U_DATABASE_HOST', 'localhost');
-define('U_DATABASE_USERNAME', 'chaptest');
-define('U_DATABASE_PASSWORD', 'chaptest');
 
 try {
     $pdo = new PDO('mysql:host='.U_DATABASE_HOST, U_DATABASE_USERNAME, U_DATABASE_PASSWORD);
@@ -17,18 +11,18 @@ try {
 
 Chaperone::setNamespace('b2b');
 Chaperone::setPDO($pdo);
-Chaperone::newSession('fred');
+Chaperone::newSession('fred@test.com');
 
 //Chaperone::attachRole('sysadmin');
 
 Chaperone::attachRole('tmcadmin', array('tmc'=>'abc'));
 Chaperone::attachRole('tmcadmin', array('tmc'=>'def'));
 
-Chaperone::attachRole('businessadmin', array('tmc'=>'ghi', 'business'=>123));
-Chaperone::attachRole('businessadmin', array('tmc'=>'ghi', 'business'=>456));
+Chaperone::attachRole('businessadmin', array('tmc'=>'ghi', 'biz'=>123));
+Chaperone::attachRole('businessadmin', array('tmc'=>'ghi', 'biz'=>456));
 
-Chaperone::attachRole('businessuser', array('tmc'=>'jkl', 'business'=>123));
-Chaperone::attachRole('businessuser', array('tmc'=>'jkl', 'business'=>456));
+Chaperone::attachRole('businessuser', array('tmc'=>'jkl', 'biz'=>123));
+Chaperone::attachRole('businessuser', array('tmc'=>'jkl', 'biz'=>456));
 
 echo 'Attached as '.Chaperone::getEmailAddress();
 ?>
